383a3c5e4e6b10c8b4ade4eab2c1a52218696c29
Aakash Chaurasia <aakashblogger2001@gmail.com>
2024-06-20 13:19:58 +0530

    Add files via upload

---
+++ 1) Operators.py
@@ -0,0 +1,93 @@
+#operators
+
+'''1. Airthmatic Operators
+2. Assignment Operators
+3. Comaperision Operators
+4. Logical Operators
+5. Identiry Operators
+6. Membership Operators
+7. Bitwise Operators'''
+
+# Airthmatic operators [+,-,/,*,%,**,//]
+
+'''
+% = modules = Shows Reminder
+** = Exponentation = Add the power on the number.
+// = Floor Division = It shows the value berfore the decimal.
+'''
+#Modules
+'''x = 45
+y = 20
+
+print("modules :-", x%y)
+print("The reminder of",x,"and",y,"is",x%y)
+'''
+
+#Exponentation
+
+'''print("The exponentation of 5 and 3 is",5**3) # 5**3 = (5*5*5)'''
+
+#Floor
+'''x,y = 19,5
+print(x//y)'''
+
+
+#--------------------------------------------------
+
+# 2) Assingment Operatiors
+
+'''
+= : Assign
+==: Equal to
++=:                    (or)     x = x + 1
+-=:                             x = x - 1
+*=:                             x = x * 1   
+/=:                             x = x / 1
+!=: Not equals to
+'''
+
+#Equals to
+'''
+x = 45
+y = 45
+print(x==y)
+'''
+#Not Equals to
+'''
+x = 100
+y = 200
+print(x!=y)
+'''
+
+#Plus Equals to (+= or x = x + 1)
+
+'''
+x = 50 
+# x = x+5
+x+=5
+x+=10
+print(x)
+'''
+
+#Minus equals to (-= or x = x - 1)
+
+# 3) Comparison operators
+
+'''>  : Greater than
+<  : Less than
+>= : Greater than equals to
+<= : Less than equals to
+== : Equals to
+!= : Not equals to
+'''
+#examples
+
+'''X = 45
+Y = 10
+
+print(X>Y)
+print(X<=Y)'''
+
+print(5%3)
+
+print(5**4300) #5*5*5*5
