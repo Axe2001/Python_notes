2a39492be3723d2a0558eb317fae43e7e7a1681b
Aakash Chaurasia <aakashblogger2001@gmail.com>
2024-06-21 21:01:07 +0530

    Add files via upload

---
+++ 2) Data_types.py
@@ -0,0 +1,105 @@
+'''
+Data type:
+
+1. Numerical data = 1. integer int, 2. Float 3. Complex eg. 21 j (complex no. ends with j)
+
+2. Text Data = 1. Strings str
+
+3. Sequence Data = list, tuple, range()
+
+
+1. List = they always being written in square brackets
+eg. = ["Data","science",343,25.55]
+
+2. Tuple = Tuble are written in round bracjet,
+
+eg = ("Data","science",343,25.55)
+
+3. Range() = it is used to print the number in a sequence
+
+Three parameters of range
+range(starting,ending,step_size)
+
+---------------------------------------------------------
+
+4. Distionary data = it is written in curly brackets
+eg. = {key: values}
+
+5. set data = it is also being written in curly brackets
+
+6. Boolen Data = True, False
+
+7. None = None'''
+
+# print("hello")
+
+# #Method 1
+# x = 40
+# t = type(x)
+# print(t)
+
+# #Method 2
+# x=40j
+# print(type(x))
+
+# #metod 3
+# print(type(10.5))
+
+# #method 4
+# print(type("Aakash"))
+
+
+#len
+# X = "Data Science"
+# print(len(X))
+# print(len(X)-1)
+
+# x = "12000"
+# print(len(x))
+
+
+
+# Conate
+# x = "Data"
+# y = "Science"
+
+# print(x+y)
+
+#Forecasting of the data :- It is used to change the data type
+
+# x= 456
+# print(type(x))
+
+# y= str(x)   #Data Type Changed
+# print(type(y))
+
+# print(y*5)  #Rept
+
+x = "123"
+
+# Change to Float
+# to Complex
+# list
+# int
+
+y = float(x)
+print(y)
+print(type(y))
+
+z = complex(x)
+print(z)
+print(type(z))
+
+a = list(x)
+print(a)
+print(type(a))
+
+b = int(x)
+print(b)
+print(type(b))
+
+c = tuple(x)
+print(c)
+print(type(c))
+
+
